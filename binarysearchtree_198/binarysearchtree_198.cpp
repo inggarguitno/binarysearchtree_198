8bf36b001281e6cd0d8dc12a52a28a47c9dd8068
inggarguitno <inggar.kinanthi.ft22@mail.umy.ac.id>
2023-07-02 05:07:19 +0700

    Add project files.

---
+++ binarysearchtree_198/binarysearchtree_198.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node
+{
+public:
+	string info;
+	Node* leftchild;
+	Node* rightchild;
+
+	Node(string i, Node * 1, Node* r)
+	{
+		info = i;
+		leftchild = 1;
+		rightchild = r;
+	}
+};
+
+class BinaryTree
